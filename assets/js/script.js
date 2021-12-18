82a6d42a9a5d9576a8265e224f244d2c69ed114d
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-18 00:27:09 -0500

    Added geocoding and oneCall and an API call function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,20 @@ var forecastIcon = document.getElementsByClassName('forecastIcon');
 var forecastTemp = document.getElementsByClassName('forecastTemp');
 var forecastWind = document.getElementsByClassName('forecastWind');
 var forecastHumidity = document.getElementsByClassName('forecastHumidity');
+var geocodingAPI = 'JIAm99s7cv9HGwGfe0zIAg3ZEQBLSobn'
+var oneCallAPI = 'ef5e2549b8aa888c5f64f0c4c89090d6';
 
 searchBtn.addEventListener('click', function (event) {
   event.preventDefault();
+  getApi();
 })
 
 function getApi() {
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall';
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
+  var geocodingUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + geocodingAPI + '&location=' + searchInput.value;
+  // var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?q=' +  + '&appid=' + oneCallAPI;
+console.log(geocodingUrl)
+  // fetch(requestUrl)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
 }

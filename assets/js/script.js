2ac3a8f3d55dba10d8a022a278284a74d060ea67
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-19 00:54:47 -0500

    Added if statement to return a console log message that no search is performed when the search field is blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,107 +35,122 @@ searchBtn.addEventListener('click', function (event) {
 
 // Function to get the latitude and longitude of city searched
 function getApi() {
-  var geocodingUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + geocodingAPI + '&location=' + searchInput.value;
+  if (searchInput.value == "") {
+    console.log('No search performed')
+  } else {
+    var geocodingUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + geocodingAPI + '&location=' + searchInput.value;
 
-  // Fetches information from mapquest's api
-  fetch(geocodingUrl)
-    .then(function (geocodeResponse) {
-      return geocodeResponse.json();
-    })
+    // Fetches information from mapquest's api
+    fetch(geocodingUrl)
+      .then(function (geocodeResponse) {
+        return geocodeResponse.json();
+      })
 
-    // Function using the retrieved latitude and longitude to retrieve weather data
-    .then(function (geocodeData) {
-      var latitude = geocodeData.results[0].locations[0].latLng.lat
-      var longitude = geocodeData.results[0].locations[0].latLng.lng
-      var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=' + oneCallAPI;
+      // Function using the retrieved latitude and longitude to retrieve weather data
+      .then(function (geocodeData) {
+        var latitude = geocodeData.results[0].locations[0].latLng.lat
+        var longitude = geocodeData.results[0].locations[0].latLng.lng
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=' + oneCallAPI;
 
-      // Fetches information from the One Call API
-      fetch(requestUrl)
-        .then(function (requestResponse) {
-          return requestResponse.json();
-        })
+        // Fetches information from the One Call API
+        fetch(requestUrl)
+          .then(function (requestResponse) {
+            return requestResponse.json();
+          })
 
-        // Function that retrieves specific data for the current date as well as the next 5 days
-        .then(function (requestData) {
-          unixDate = requestData.current.dt;
-          convertedDate = moment.unix(unixDate).format('M/D/YYYY');
-          var currentWeatherIconCode = requestData.current.weather[0].icon;
-          var currentTempOutput = requestData.current.temp;
-          var currentWindOutput = requestData.current.wind_speed;
-          var currentHumidityOutput = requestData.current.humidity;
-          var uvIndex = requestData.current.uvi;
-
-          // Outputs all data variables to the HTML
-          searchedCity.innerHTML = searchInput.value.toLowerCase();
-          currentDate.innerHTML = '(' + convertedDate + ')';
-          currentIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + currentWeatherIconCode + '.png')
-          currentTemp.innerHTML = 'Temp: ' + currentTempOutput + ' &#xb0;F';
-          currentWind.innerHTML = 'Wind: ' + currentWindOutput + ' MPH';
-          currentHumidity.innerHTML = 'Humidity: ' + currentHumidityOutput + ' %';
-          currentUV.innerHTML = uvIndex;
-
-          // Sets class currentUV span element to a specific class depending on what UV index was returned from One Call API
-          if (uvIndex < 3) {
-            currentUV.setAttribute('class', 'lowUV');
-          } else if (uvIndex >= 3 && uvIndex < 6) {
-            currentUV.setAttribute('class', 'moderateUV');
-          } else if (uvIndex >= 6 && uvIndex < 8) {
-            currentUV.setAttribute('class', 'highUV');
-          } else if (uvIndex >= 8 && uvIndex < 11) {
-            currentUV.setAttribute('class', 'veryHighUV');
-          } else {
-            currentUV.setAttribute('class', 'extremeUV');
-          };
-
-          // For loop to retrieve specific data for the 5 day forecast
-          for (var i = 1; i < 6; i++) {
-            unixDate = requestData.daily[i].dt;
+          // Function that retrieves specific data for the current date as well as the next 5 days
+          .then(function (requestData) {
+            unixDate = requestData.current.dt;
             convertedDate = moment.unix(unixDate).format('M/D/YYYY');
-            var forecastWeatherIconCode = requestData.daily[i].weather[0].icon;
-            var forecastTempOutput = requestData.daily[i].temp.day;
-            var forecastWindOutput = requestData.daily[i].wind_speed;
-            var forecastHumidityOutput = requestData.daily[i].humidity;
+            var currentWeatherIconCode = requestData.current.weather[0].icon;
+            var currentTempOutput = requestData.current.temp;
+            var currentWindOutput = requestData.current.wind_speed;
+            var currentHumidityOutput = requestData.current.humidity;
+            var uvIndex = requestData.current.uvi;
 
-            // Outputs data for each forecast date in their respective cards on the HTML
-            forecastDate[i - 1].innerHTML = convertedDate;
-            forecastIcon[i - 1].setAttribute('src', 'http://openweathermap.org/img/w/' + forecastWeatherIconCode + '.png');
-            forecastTemp[i - 1].innerHTML = 'Temp: ' + forecastTempOutput + ' &#xb0;F';
-            forecastWind[i - 1].innerHTML = 'Wind: ' + forecastWindOutput + ' MPH';
-            forecastHumidity[i - 1].innerHTML = 'Humidity: ' + forecastHumidityOutput + ' %';
-          };
+            // Outputs all data variables to the HTML
+            searchedCity.innerHTML = searchInput.value.toLowerCase();
+            currentDate.innerHTML = '(' + convertedDate + ')';
+            currentIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + currentWeatherIconCode + '.png')
+            currentTemp.innerHTML = 'Temp: ' + currentTempOutput + ' &#xb0;F';
+            currentWind.innerHTML = 'Wind: ' + currentWindOutput + ' MPH';
+            currentHumidity.innerHTML = 'Humidity: ' + currentHumidityOutput + ' %';
+            currentUV.innerHTML = uvIndex;
 
-          // Sets the weather dashboard to visible if it is hidden (new page instance)
-          weatherDashboard.style.visibility = 'visible';
-        })
+            // Sets class currentUV span element to a specific class depending on what UV index was returned from One Call API
+            if (uvIndex < 3) {
+              currentUV.setAttribute('class', 'lowUV');
+            } else if (uvIndex >= 3 && uvIndex < 6) {
+              currentUV.setAttribute('class', 'moderateUV');
+            } else if (uvIndex >= 6 && uvIndex < 8) {
+              currentUV.setAttribute('class', 'highUV');
+            } else if (uvIndex >= 8 && uvIndex < 11) {
+              currentUV.setAttribute('class', 'veryHighUV');
+            } else {
+              currentUV.setAttribute('class', 'extremeUV');
+            };
 
-        // Saves weather dashboard snapshot for the specific city and the specific time it was searched
-        .then(function () {
-          var dashboardEl = weatherDashboard.innerHTML;
-          localStorage.setItem(searchInput.value.toLowerCase(), dashboardEl);
-        })
+            // For loop to retrieve specific data for the 5 day forecast
+            for (var i = 1; i < 6; i++) {
+              unixDate = requestData.daily[i].dt;
+              convertedDate = moment.unix(unixDate).format('M/D/YYYY');
+              var forecastWeatherIconCode = requestData.daily[i].weather[0].icon;
+              var forecastTempOutput = requestData.daily[i].temp.day;
+              var forecastWindOutput = requestData.daily[i].wind_speed;
+              var forecastHumidityOutput = requestData.daily[i].humidity;
 
-        // Function for adding the searched city to the historical search button list
-        .then(function () {
-          var historicalSearchEl = document.createElement('button');
-          var removedSpacesId = searchInput.value.toLowerCase().replace(/\s+/g, '')
-          var checkEl = document.getElementById(removedSpacesId);
+              // Outputs data for each forecast date in their respective cards on the HTML
+              forecastDate[i - 1].innerHTML = convertedDate;
+              forecastIcon[i - 1].setAttribute('src', 'http://openweathermap.org/img/w/' + forecastWeatherIconCode + '.png');
+              forecastTemp[i - 1].innerHTML = 'Temp: ' + forecastTempOutput + ' &#xb0;F';
+              forecastWind[i - 1].innerHTML = 'Wind: ' + forecastWindOutput + ' MPH';
+              forecastHumidity[i - 1].innerHTML = 'Humidity: ' + forecastHumidityOutput + ' %';
+            };
 
-          // Checks if an id exists already for new searched city and if it returns null (non-existent), continues with the function
-          if (checkEl == null) {
+            // Sets the weather dashboard to visible if it is hidden (new page instance)
+            weatherDashboard.style.visibility = 'visible';
+          })
 
-            // If there are less than 7 existing buttons, adds new button at the top of the list for the searched city
-            if (historicalSearch.children.length < 7) {
-              historicalSearch.prepend(historicalSearchEl);
-              historicalSearch.firstChild.type = 'button';
-              historicalSearch.firstChild.className = 'btn btn-primary';
-              historicalSearch.firstChild.id = removedSpacesId;
-              historicalSearch.firstChild.innerHTML = searchInput.value.toLowerCase();
-              localStorage.setItem('historicalSearch', historicalSearch.innerHTML);
+          // Saves weather dashboard snapshot for the specific city and the specific time it was searched
+          .then(function () {
+            var dashboardEl = weatherDashboard.innerHTML;
+            localStorage.setItem(searchInput.value.toLowerCase(), dashboardEl);
+          })
+
+          // Function for adding the searched city to the historical search button list
+          .then(function () {
+            var historicalSearchEl = document.createElement('button');
+            var removedSpacesId = searchInput.value.toLowerCase().replace(/\s+/g, '')
+            var checkEl = document.getElementById(removedSpacesId);
+
+            // Checks if an id exists already for new searched city and if it returns null (non-existent), continues with the function
+            if (checkEl == null) {
+
+              // If there are less than 7 existing buttons, adds new button at the top of the list for the searched city
+              if (historicalSearch.children.length < 7) {
+                historicalSearch.prepend(historicalSearchEl);
+                historicalSearch.firstChild.type = 'button';
+                historicalSearch.firstChild.className = 'btn btn-primary';
+                historicalSearch.firstChild.id = removedSpacesId;
+                historicalSearch.firstChild.innerHTML = searchInput.value.toLowerCase();
+                localStorage.setItem('historicalSearch', historicalSearch.innerHTML);
+              }
+
+              // If there are 7 existing buttons, removes the oldest (last) button and adds new button at the top of the list for the searched city
+              else {
+                historicalSearch.removeChild(historicalSearch.lastChild);
+                historicalSearch.prepend(historicalSearchEl);
+                historicalSearch.firstChild.type = 'button';
+                historicalSearch.firstChild.className = 'btn btn-primary';
+                historicalSearch.firstChild.id = removedSpacesId;
+                historicalSearch.firstChild.innerHTML = searchInput.value.toLowerCase();
+                localStorage.setItem('historicalSearch', historicalSearch.innerHTML);
+              };
             }
 
-            // If there are 7 existing buttons, removes the oldest (last) button and adds new button at the top of the list for the searched city
+            // If the check returns true (id does exist), removes the element the id exists on and adds the new search to the top of the list
             else {
-              historicalSearch.removeChild(historicalSearch.lastChild);
+              checkEl.remove();
               historicalSearch.prepend(historicalSearchEl);
               historicalSearch.firstChild.type = 'button';
               historicalSearch.firstChild.className = 'btn btn-primary';
@@ -143,20 +158,9 @@ function getApi() {
               historicalSearch.firstChild.innerHTML = searchInput.value.toLowerCase();
               localStorage.setItem('historicalSearch', historicalSearch.innerHTML);
             };
-          }
-
-          // If the check returns true (id does exist), removes the element the id exists on and adds the new search to the top of the list
-          else {
-            checkEl.remove();
-            historicalSearch.prepend(historicalSearchEl);
-            historicalSearch.firstChild.type = 'button';
-            historicalSearch.firstChild.className = 'btn btn-primary';
-            historicalSearch.firstChild.id = removedSpacesId;
-            historicalSearch.firstChild.innerHTML = searchInput.value.toLowerCase();
-            localStorage.setItem('historicalSearch', historicalSearch.innerHTML);
-          };
-        });
-    });
+          });
+      });
+  };
 };
 
 // Test Code for retrieving local storage and setting it to page
